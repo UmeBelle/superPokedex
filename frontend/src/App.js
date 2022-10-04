aa5c8570115405c8593ba830411523980f44f795
Tadeo Barzelli <barzellitadeo@gmail.com>
2022-10-04 16:07:29 -0300

    Se agregan datos al componente card desde el backend, se corrijen estilos. (me olvide de cambiar de branch perdon no va a volver a pasar u.u)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,7 +22,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<RegisterComponent />}></Route>
         <Route path="/pokedex" element={<Pokedex/>}></Route>
-        <Route path=":nombre" element={<Card />}></Route>
+        <Route path="/:name" element={<Card />}></Route>
         <Route path="/login" element={<LoginComponent />}></Route>
         <Route path="*" element={<Navigate to='/register' replace />}></Route>
       </Routes>
